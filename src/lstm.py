24e83988f32199310e673fef1d09a7c0238d4582
Zeerak Waseem <zeerakw@laptop>
2019-10-02 11:06:19 +0100

    Add layers argument lstm

--- src/lstm.py
+++ src/lstm.py
@@ -3,12 +3,13 @@ import torch.nn as nn
 
 class LSTMClassifier(nn.Module):
 
-    def __init(self, hidden_dim: int, input_dim: int, embedding_dim: int, no_classes: int):
+    def __init(self, hidden_dim: int, input_dim: int, embedding_dim: int, no_classes: int, no_layers: int,:
         """Initialise the LSTM.
         :param hidden_dim: The dimensionality of the hidden dimension.
         :param input_dim: The dimensionality of the input to the embedding generation.
         :param embedding_dim: The dimensionality of the the produced embeddings.
         :param no_classes: Number of classes for to predict on.
+        :param no_layers: The number of recurrent layers in the LSTM (1-3).
         """
         super(LSTMClassifier, self).__init__()
 
@@ -17,7 +18,7 @@ class LSTMClassifier(nn.Module):
 
         # Define layers of the network
         self.embeddings = nn.Embedding(input_dim, embedding_dim)
-        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, no_layers)
         self.to_output = nn.Linear(hidden_dim, no_classes)
 
         # Set the method for producing "probability" distribution.
