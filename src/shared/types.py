e3f507372c5118d9f5c26e07b0b219b8e8f3aff8
Zeerak Waseem <zeerakw@laptop>
2019-10-07 16:09:12 +0100

    Just some sorting

--- src/shared/types.py
+++ src/shared/types.py
@@ -2,11 +2,16 @@ import torch
 import spacy
 import numpy as np
 from torch.nn import Module
+from torchtext.data import Field
 from sklearn.base import ClassifierMixin, TransformerMixin
 from typing import Union
 
 
+# Data types
+FieldType = Field
 NPData = Union[list, np.ndarray, torch.LongTensor]
+DocType = Union[str, list, spacy.tokens.doc.Doc]
+
+# Model/Vectorizer Type
 ModelType = Union[ClassifierMixin, Module]
 VectType = TransformerMixin
-DocType = Union[str, list, spacy.tokens.doc.Doc]
