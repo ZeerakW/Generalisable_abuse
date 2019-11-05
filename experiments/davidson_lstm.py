f3cfb541e4f241f57ad5a096991baa4847390c95
Zeerak Waseem <zeerakw@laptop>
2019-11-05 15:54:38 +0000

    LSTM working

--- experiments/davidson_lstm.py
+++ experiments/davidson_lstm.py
@@ -7,10 +7,10 @@ import torch.optim as optim
 from torchtext.data import TabularDataset, BucketIterator, Field
 sys.path.extend(['/Users/zeerakw/Documents/PhD/projects/Generalisable_abuse'])
 
-from gen.shared.data import BatchGenerator
+from gen.shared.data import OnehotBatchGenerator
 from gen.neural import LSTMClassifier
 from gen.shared.clean import Cleaner
-from gen.shared.train import compute_unigram_liwc
+# from gen.shared.train import compute_unigram_liwc
 
 text_label = Field(sequential = True,
                    include_lengths = False,
@@ -26,6 +26,7 @@ int_label = Field(sequential = False,
 
 device = 'cpu'
 data_dir = '/Users/zeerakw/Documents/PhD/projects/Generalisable_abuse/data/'
+# data_file = 'davidson_offensive.csv'
 data_file = 'davidson_test.csv'
 path = os.path.join(data_dir, data_file)
 file_format = 'csv'
@@ -44,16 +45,17 @@ fields = [('', None), ('CF_count', None), ('hate_speech', None), ('offensive', N
 
 data = TabularDataset(path, format = file_format, fields = fields, skip_header = True)
 train, test = data.split(split_ratio = 0.8, stratified = True)
-loaded = (train, test)
 text_field.build_vocab(train)
+VOCAB_SIZE = len(text_field.vocab)
+loaded = (train, test)
 
 print("Vocab Size", len(text_field.vocab))
 
-batch_sizes = (64, 32)
+batch_sizes = (64, 64)
 tmp_train, tmp_test = BucketIterator.splits(loaded, batch_sizes = batch_sizes, sort_key = lambda x: len(x.text),
                                             device = device, shuffle = True, repeat = False)
-train_batches = BatchGenerator(tmp_train, 'text', 'label')
-test_batches = BatchGenerator(tmp_test, 'text', 'label')
+train_batches = OnehotBatchGenerator(tmp_train, 'text', 'label', VOCAB_SIZE)
+test_batches = OnehotBatchGenerator(tmp_test, 'text', 'label', VOCAB_SIZE)
 
 model = LSTMClassifier(len(text_field.vocab), embedding_dim = 128, hidden_dim = 128, no_classes = 3, no_layers = 1)
 optimizer = optim.Adam(model.parameters(), lr = 0.01)
@@ -66,8 +68,9 @@ def train(model, epochs, batches, loss_func, optimizer):
         epoch_loss = []
         model.zero_grad()
         for X, y in batches:
+            X = X.float()
             scores = model(X)
-            loss = loss_func(scores[0], y)
+            loss = loss_func(scores, y)
             loss.backward()
             optimizer.step()
             epoch_loss.append(float(loss))
