0e2040b357182248f55d2323b595a637eb180f50
Zeerak Waseem <zeerakw@laptop>
2019-11-05 15:44:53 +0000

    change filenames

--- experiments/davidson_lstm.py
+++ experiments/davidson_lstm.py
@@ -1,65 +1,80 @@
-import sys, pdb
+import os
+import sys
+import numpy as np
+import torch.nn as nn
+from tqdm import tqdm
+import torch.optim as optim
+from torchtext.data import TabularDataset, BucketIterator, Field
 sys.path.extend(['/Users/zeerakw/Documents/PhD/projects/Generalisable_abuse'])
 
-from tqdm import tqdm
-import torch.nn as nn
-import torch.optim as optim
-import gen.shared.types as t
-from gen.shared.clean import Cleaner
-from gen.shared.train import create_batches, compute_unigram_liwc
+from gen.shared.data import BatchGenerator
 from gen.neural import LSTMClassifier
+from gen.shared.clean import Cleaner
+from gen.shared.train import compute_unigram_liwc
 
+text_label = Field(sequential = True,
+                   include_lengths = False,
+                   use_vocab = True,
+                   pad_token = "<PAD>",
+                   unk_token = "<UNK>")
 
-def setup_data():
-    device = 'cpu'
-    data_dir = '/Users/zeerakw/Documents/PhD/projects/Generalisable_abuse/data/'
-    cleaning = ['lower', 'url', 'hashtag', 'username']
-    clean = Cleaner(cleaning)
+int_label = Field(sequential = False,
+                  include_lengths = False,
+                  use_vocab = False,
+                  pad_token = None,
+                  unk_token = None)
 
-    # Davidson
-    text = (t.text_data, {'attribute': ['tokenize', 'preprocessing'],
-                          'value': [clean.tokenize, compute_unigram_liwc]})
-    # text = (t.text_data, {'attribute': ['tokenize'],
-    #                       'value': [clean.tokenize]})
-    label = (t.int_label, None)
+device = 'cpu'
+data_dir = '/Users/zeerakw/Documents/PhD/projects/Generalisable_abuse/data/'
+data_file = 'davidson_test.csv'
+path = os.path.join(data_dir, data_file)
+file_format = 'csv'
+cleaners = ['lower', 'url', 'hashtag', 'username']
+clean = Cleaner(cleaners)
 
-    # dict_fields = {'': None, kk}
+# Set fields
+text_field = text_label
+label_field = int_label
 
-    fields = [('empty', None), ('CF_count', None), ('hate_speech', None), ('offensive', None), ('neither', None),
-              ('label', label[0]), ('data', text[0])]
+# Update training field
+setattr(text_field, 'tokenize', clean.tokenize)
+# setattr(text_field, 'preprocessing', compute_unigram_liwc)
+fields = [('', None), ('CF_count', None), ('hate_speech', None), ('offensive', None), ('neither', None),
+          ('label', label_field), ('text', text_field)]
 
-    data_opts = {'splits': {'train': 'davidson_offensive.csv'}, 'ftype': 'csv', 'data_field': text, 'fields': fields,
-                 'label_field': label, 'batch_sizes': (64,), 'shuffle': True, 'skip_header': True,
-                 'repeat_in_batches': False, 'split_ratio': 0.8}
+data = TabularDataset(path, format = file_format, fields = fields, skip_header = True)
+train, test = data.split(split_ratio = 0.8, stratified = True)
+loaded = (train, test)
+text_field.build_vocab(train)
 
-    ds = create_batches(data_dir = data_dir, device = device, **data_opts)
+print("Vocab Size", len(text_field.vocab))
 
-    return ds
+batch_sizes = (64, 32)
+tmp_train, tmp_test = BucketIterator.splits(loaded, batch_sizes = batch_sizes, sort_key = lambda x: len(x.text),
+                                            device = device, shuffle = True, repeat = False)
+train_batches = BatchGenerator(tmp_train, 'text', 'label')
+test_batches = BatchGenerator(tmp_test, 'text', 'label')
 
-
-def train(epochs, data, model, loss_func, optimizer):
-
-    for epoch in tqdm(range(epochs)):
-        model.zero_grad()
-        batch = next(iter(train))
-
-        scores = model(batch)
-        loss = loss_func(scores, batch.labels)
-        loss.backward()
-        optimizer.step()
-
-
-# TODO Implement metrics
-# TODO Get class of the predicted label
-ds, train_batch, dev_batch, test_batch, vocab = setup_data()
-
-HIDDEN_DIM = 300
-NO_CLASSES = 3
-NO_LAYERS = 2
-VOCAB_SIZE = len(vocab)
-
-model = LSTMClassifier(hidden_dim = HIDDEN_DIM, input_dim = VOCAB_SIZE, no_classes = NO_CLASSES, no_layers = NO_LAYERS)
+model = LSTMClassifier(len(text_field.vocab), embedding_dim = 128, hidden_dim = 128, no_classes = 3, no_layers = 1)
 optimizer = optim.Adam(model.parameters(), lr = 0.01)
 loss = nn.NLLLoss()
 
-train(300, train_batch, model, loss, optimizer)
+
+def train(model, epochs, batches, loss_func, optimizer):
+    losses = []
+    for epoch in tqdm(range(epochs)):
+        epoch_loss = []
+        model.zero_grad()
+        for X, y in batches:
+            scores = model(X)
+            loss = loss_func(scores[0], y)
+            loss.backward()
+            optimizer.step()
+            epoch_loss.append(float(loss))
+        losses.append(np.mean(epoch_loss))
+
+    print("Max loss: {0};Index: {1}\nMin loss: {2}; Index: {3}".format(np.max(losses), np.argmax(losses),
+                                                                       np.min(losses), np.argmin(losses)))
+
+
+train(model, 5, train_batches, loss, optimizer)
