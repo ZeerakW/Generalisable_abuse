6d61d47e201d59daf60aa48df7a5595951f40eaf
Zeerak Waseem <zeerakw@laptop>
2019-11-12 15:05:25 +0000

    move batching and field saving into separate file

--- gen/shared/train.py
+++ gen/shared/train.py
@@ -1,8 +1,9 @@
 import re
 import pdb
-from string import punctuation
+import numpy as np
+from tqdm import tqdm
 import gen.shared.types as t
-from gen.shared.data import Dataset, BatchGenerator
+from string import punctuation
 
 
 def read_liwc() -> dict:
@@ -21,17 +22,13 @@ def read_liwc() -> dict:
 global liwc_dict
 liwc_dict = read_liwc()
 
-# TODO Different ways of representing LIWC
-# First category
-# Take counts of categories, use the heighest count
-# Basically look at the if statement and see what comes out there.
-
 
 def compute_unigram_liwc(doc: t.DocType):
     """Compute LIWC for each document document.
     :param doc (t.DocType): Document to operate on.
     :return liwc_doc (t.DocType): Document represented as LIWC categories.
     """
+    # TODO modify
     liwc_doc = []
     kleene_star = [k[:-1] for k in liwc_dict if k[-1] == '*']
 
@@ -51,7 +48,7 @@ def compute_unigram_liwc(doc: t.DocType):
             elif num_cands == 1:
                 term = candidates[0]
             elif num_cands > 1:
-                sorted_cands = sorted(candidates, key=len, reverse = True)
+                sorted_cands = sorted(candidates, key=len, reverse = True)  # Longest first
                 term = sorted_cands[0] + '*'
             liwc_doc.append(term)
     try:
@@ -62,59 +59,18 @@ def compute_unigram_liwc(doc: t.DocType):
     return " ".join(liwc_doc)
 
 
-def store_fields(obj, data_field, label_field, **kwargs):
-    """Store fields in the dataset object. Final two fields always label, train.
-    :param data (t.FieldType): The field instance for the data.
-    :param label (t.FieldType): The field instance for the label.
-    :param kwargs: Will search for any fields in this.
-    """
-    field = []
-    if kwargs:
-        for k in kwargs:
-            if 'field' in k:
-                field.append(kwargs[k])
-    field.extend([label_field, data_field])
-    obj.field_instance = tuple(field)
+def train(model, epochs, batches, loss_func, optimizer, text_field):
+    losses = []
+    for epoch in tqdm(range(epochs)):
+        epoch_loss = []
+        model.zero_grad()
+        for X, y in batches:
+            scores = model(X)
+            loss = loss_func(scores, y)
+            loss.backward()
+            optimizer.step()
+            epoch_loss.append(float(loss))
+        losses.append(np.mean(epoch_loss))
 
-
-def create_batches(data_dir: str, splits: t.Dict[str, t.Union[str, None]], ftype: str, fields: t.Union[dict, list],
-                   batch_sizes: t.Tuple[int, ...], shuffle: bool, skip_header: bool, repeat_in_batches: bool,
-                   device: t.Union[str, int], data_field: t.Tuple[t.FieldType, t.Union[t.Dict, None]],
-                   label_field: t.Tuple[t.FieldType, t.Union[t.Dict, None]], **kwargs):
-
-    # Initiate the dataset object
-    data = Dataset(data_dir = data_dir, splits = splits, ftype = ftype, fields = fields,
-                   shuffle = shuffle, skip_header = skip_header, repeat_in_batches = repeat_in_batches,
-                   device = device)
-
-    # If the fields need new attributes set: set them.
-    # TODO assumes only data and field labels need modification.
-    if data_field[1]:
-        data.set_field_attribute([data_field[0]], data_field[1]['attribute'], data_field[1]['value'])
-
-    if label_field[1]:
-        data.set_field_attribute([label_field[0]], label_field[1]['attribute'], label_field[1]['value'])
-
-    # Store our Field instances so we can later access them.
-    store_fields(data, data_field, label_field, **kwargs)
-
-    data.fields_obj = fields  # Update the fields in the class
-
-    loaded = data.load_data()  # Data paths exist in the class
-
-    if len([v for v in splits.values() if v is not None]) == 1:  # If only one dataset is given
-        strata_label = kwargs['label'] if 'label' in kwargs else 'label'
-        pdb.set_trace()
-        train, test = data.split(split_ratio = kwargs['split_ratio'], stratified = True, strata_field = strata_label)
-        loaded = (train, None, test)
-
-    data_field.build_vocab()
-    label_field.build_vocab()
-
-    train, dev, test = data.generate_batches(lambda x: len(x.data), loaded, batch_sizes)
-    train_batch = BatchGenerator(train, 'data', 'label')
-    dev_batch = None if dev is None else BatchGenerator(dev, 'data', 'label')
-    test_batch = BatchGenerator(test, 'data', 'label')
-
-    batches = (train_batch, dev_batch, test_batch)
-    return data, batches, data_field.vocab
+    print("Max loss: {0};Index: {1}\nMin loss: {2}; Index: {3}".format(np.max(losses), np.argmax(losses),
+                                                                       np.min(losses), np.argmin(losses)))
