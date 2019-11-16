Move the liwc computation and run evaluation on correct stuff

experiments/davidson_cnn.py
@@ -8,7 +8,8 @@ sys.path.extend(['/Users/zeerakw/Documents/PhD/projects/Generalisable_abuse'])
 from gen.shared.data import OnehotBatchGenerator
 from gen.neural import CNNClassifier
 from gen.shared.clean import Cleaner
-from gen.shared.train import compute_unigram_liwc, train
+from gen.shared.train import compute_unigram_liwc, train_model, evaluate_model
+from sklearn.metrics import accuracy_score
 
 text_label = Field(sequential = True,
                    include_lengths = False,
@@ -24,8 +25,11 @@ int_label = Field(sequential = False,
 
 device = 'cpu'
 data_dir = '/Users/zeerakw/Documents/PhD/projects/Generalisable_abuse/data/'
-data_file = 'davidson_offensive.csv'
-path = os.path.join(data_dir, data_file)
+# train_file = 'davidson_offensive.csv'
+train_file = 'davidson_train.csv'
+test_file = 'davidson_test.csv'
+train_path = os.path.join(data_dir, train_file)
+test_path = os.path.join(data_dir, test_file)
 file_format = 'csv'
 cleaners = ['lower', 'url', 'hashtag', 'username']
 clean = Cleaner(cleaners)
@@ -40,10 +44,11 @@ setattr(text_field, 'preprocessing', compute_unigram_liwc)
 fields = [('', None), ('CF_count', None), ('hate_speech', None), ('offensive', None), ('neither', None),
           ('label', label_field), ('text', text_field)]
 
-data = TabularDataset(path, format = file_format, fields = fields, skip_header = True)
-train_data, test = data.split(split_ratio = 0.8, stratified = True)
-loaded = (train_data, test)
-text_field.build_vocab(train_data)
+data = TabularDataset(train_path, format = file_format, fields = fields, skip_header = True)
+test = TabularDataset(test_path, format = file_format, fields = fields, skip_header = True)
+train_data, valid = data.split(split_ratio = 0.9, stratified = True)
+loaded = (train_data, valid)
+text_field.build_vocab(data)
 VOCAB_SIZE = len(text_field.vocab)
 
 print("Vocab Size", len(text_field.vocab))
@@ -59,4 +64,5 @@ model = CNNClassifier(window_sizes = [2, 3, 4], num_filters = 3, max_feats = VOC
 optimizer = optim.Adam(model.parameters(), lr = 0.01)
 loss = nn.NLLLoss()
 
-train(model, 5, train_batches, loss, optimizer, text_field)
+train_model(model, 5, train_batches, loss, optimizer, text_field)
+evaluate_model(model, test_batches, loss, accuracy_score, "accuracy")

experiments/davidson_lstm.py
@@ -8,7 +8,9 @@ sys.path.extend(['/Users/zeerakw/Documents/PhD/projects/Generalisable_abuse'])
 from gen.shared.data import OnehotBatchGenerator
 from gen.neural import LSTMClassifier
 from gen.shared.clean import Cleaner
-from gen.shared.train import compute_unigram_liwc, train
+from gen.shared.representations import compute_unigram_liwc
+from gen.shared.train import train_model, evaluate_model
+from sklearn.metrics import accuracy_score
 
 text_label = Field(sequential = True,
                    include_lengths = False,
@@ -59,4 +61,5 @@ model = LSTMClassifier(len(text_field.vocab), embedding_dim = 128, hidden_dim = 
 optimizer = optim.Adam(model.parameters(), lr = 0.01)
 loss = nn.NLLLoss()
 
-train(model, 5, train_batches, loss, optimizer)
+train_model(model, 5, train_batches, loss, optimizer)
+evaluate_model(model, test_batches, loss, accuracy_score, "accuracy")

experiments/davidson_mlp.py
@@ -8,7 +8,9 @@ sys.path.extend(['/Users/zeerakw/Documents/PhD/projects/Generalisable_abuse'])
 from gen.shared.data import OnehotBatchGenerator
 from gen.neural import MLPClassifier
 from gen.shared.clean import Cleaner
-from gen.shared.train import compute_unigram_liwc, train
+from gen.shared.representations import compute_unigram_liwc
+from gen.shared.train import train_model, evaluate_model
+from sklearn.metrics import accuracy_score
 
 text_label = Field(sequential = True,
                    include_lengths = False,
@@ -59,4 +61,5 @@ model = MLPClassifier(len(text_field.vocab), hidden_dim = 128, output_dim = 3)
 optimizer = optim.Adam(model.parameters(), lr = 0.01)
 loss = nn.NLLLoss()
 
-train(model, 30, train_batches, loss, optimizer, text_field)
+train_model(model, 30, train_batches, loss, optimizer, text_field)
+evaluate_model(model, test_batches, loss, accuracy_score, "accuracy")