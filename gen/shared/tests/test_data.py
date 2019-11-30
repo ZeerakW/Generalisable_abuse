b8e125a59fe6564236020d3a81d6b64e5fecef46
Zeerak Waseem <zeerakw@laptop>
2019-11-30 15:18:30 +0000

    add tests

--- gen/shared/tests/test_data.py
+++ gen/shared/tests/test_data.py
@@ -1,7 +1,5 @@
 import unittest
-from string import punctuation
 from src.shared.data import Field, GeneralDataset, Datapoint
-from src.shared import types
 
 
 class TestDataSet(unittest.TestCase):
@@ -9,10 +7,6 @@ class TestDataSet(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Set up class with data as well."""
-        cls.data = [("me gusta comer en la cafeteria", "SPANISH"),
-                    ("Give it to me", "ENGLISH"),
-                    ("No creo que sea una buena idea", "SPANISH"),
-                    ("No it is not a good idea to get lost at sea", "ENGLISH")]
         fields = [Field('text', train = True, label = False, ignore = False, ix = 0, cname = 'text'),
                   Field('label', train = True, label = True)]
 
@@ -27,6 +21,12 @@ class TestDataSet(unittest.TestCase):
                                           lower = True, preprocessor = None, transformations = None,
                                           label_processor = None)
 
+    @classmethod
+    def tearDown(cls):
+        """Tear down class."""
+        cls.csv_dataset = 0
+        cls.json_dataset = 0
+
     def test_load(self):
         """Test dataset loading."""
         expected = [("me gusta comer en la cafeteria", "SPANISH"),
@@ -39,7 +39,8 @@ class TestDataSet(unittest.TestCase):
 
         json_train = self.json_dataset.load('train', skip_header = True)
         json_output = [(doc.text, doc.label) for doc in json_train]
-        self.assertListEqual(json_output, expected)
+        self.assertListEqual(json_output, expected, msg = "Data Loading failed.")
+        self.assertIsInstance(json_output[0], Datapoint, msg = "Data Loading failed gave wrong type.")
 
     def test_build_vocab(self):
         """Test vocab building method."""
@@ -99,6 +100,7 @@ class TestDataSet(unittest.TestCase):
         self.assertEqual(self.csv_dataset.label_ix_lookup(0), 'SPANISH', msg = "label ix lookup failed.")
 
     def test_label_count(self):
+        """Test label size is expected."""
         train = self.csv_dataset.load('train')
         self.csv_dataset.build_label(train)
         self.assertEqual(self.csv_dataset.label_count(), 2, msg = 'Test that label count matches labels failed.')
@@ -115,5 +117,48 @@ class TestDataSet(unittest.TestCase):
         result = self.csv_dataset.process_label('SPANISH', processor = lambda x: 1)
         self.assertEqual(result, expected, msg = "Labelprocessor failed with custom processor")
 
-    def test_load_label(self):
-        self.assertEqual(True, False)
+    def test_process_doc(self):
+        """Test document processing."""
+        setattr(self.csv_dataset, 'lower', False)
+        setattr(self.csv_dataset, 'preprocessor', None)
+        setattr(self.csv_dataset, 'repr_transform', None)
+
+        inputs = "Give it to me, baby. Uhuh! Uhuh!"
+        expected = inputs.split()
+        output = self.csv_dataset.process_doc(inputs)
+        self.assertListEqual(output, expected, msg = "Process Document failed.")
+        self.assertIsInstance(output, list, msg = "Process document returned wrong type.")
+
+        expected = inputs.lower().split()
+        setattr(self.csv_dataset, 'lower', True)
+        output = self.csv_dataset.process_doc(inputs)
+        self.assertListEqual(output, expected, msg = "Process Document failed with lowercasing.")
+        self.assertIsInstance(output, list, msg = "Process document with lowercasing produces wrong type.")
+
+        output = self.csv_dataset.process_doc(inputs.split())
+        self.assertListEqual(output, expected, msg = "Process Document failed with input type list.")
+        self.assertIsInstance(output, list, msg = "Process document with input type list produces wrong type.")
+
+        setattr(self.csv_dataset, 'preprocessor', lambda x: ["TEST" if '!' in tok else tok for tok in x])
+        expected = ["TEST" if '!' in tok else tok for tok in inputs.split()]
+        output = self.csv_dataset.process_doc(inputs)
+        self.assertListEqual(output, expected, msg = "Process Document failed with preprocessor.")
+        self.assertIsInstance(output, list, msg = "Process Document with preprocessor returned wrong type.")
+
+        def transform(doc):
+            transform = {'give': 'VERB', 'it': 'DET', 'to': "PREP", 'me': "PRON", 'uhuh!': 'AGREEMENT',
+                         'uhuh': 'AGREEMENT'}
+            for w in doc:
+                yield transform[w.lower()]
+
+        setattr(self.csv_dataset, 'repr_transform', transform)
+        expected = "VERB DET PREP PRON AGREEMENT AGREEMENT".split()
+        output = self.csv_dataset.process_doc(inputs)
+        self.assertListEqual(output, expected, msg = "Process Document failed with represntation transformation.")
+        self.assertIsInstance(output, list, msg = "Process Document with representation transformation returned wrong"
+                                                   "type.")
+
+    def test_pad(self):
+        """Test padding of document."""
+        train = self.csv_dataset.load('train')
+        train = self.csv_dataset.pad(train, length = 10)
