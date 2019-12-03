8ef5e7729b5d50c6ee622f3721e4cfba05be9de9
Zeerak Waseem <zeerakw@laptop>
2019-12-03 23:15:23 +0100

    Move train variable to setupclass

--- gen/shared/tests/test_data.py
+++ gen/shared/tests/test_data.py
@@ -1,5 +1,6 @@
 import unittest
 from gen.shared.data import Field, GeneralDataset, Datapoint
+import pdb
 
 
 class TestDataSet(unittest.TestCase):
@@ -8,13 +9,15 @@ class TestDataSet(unittest.TestCase):
     def setUpClass(cls):
         """Set up class with data as well."""
         fields = [Field('text', train = True, label = False, ignore = False, ix = 0, cname = 'text'),
-                  Field('label', train = True, label = True)]
+                  Field('label', train = False, label = True, cname = 'label', ignore = False, ix = 1)]
 
         cls.csv_dataset = GeneralDataset(data_dir = '~/PhD/projects/active/Generalisable_abuse/gen/shared/tests/',
                                          ftype = 'csv', fields = fields, train = 'train.csv', dev = None,
                                          test = 'test.csv', train_labels = None, tokenizer = lambda x: x.split(),
                                          lower = True, preprocessor = None, transformations = None,
                                          label_processor = None, sep = ',')
+        cls.csv_dataset.load('train')
+        cls.train = cls.csv_dataset.data
         cls.json_dataset = GeneralDataset(data_dir = '~/PhD/projects/active/Generalisable_abuse/gen/shared/tests/',
                                           ftype = 'json', fields = fields, train = 'train.json', dev = None,
                                           test = 'test.json', train_labels = None, tokenizer = lambda x: x.split(),
@@ -29,25 +32,25 @@ class TestDataSet(unittest.TestCase):
 
     def test_load(self):
         """Test dataset loading."""
-        expected = [("me gusta comer en la cafeteria", "SPANISH"),
-                    ("Give it to me", "ENGLISH"),
-                    ("No creo que sea una buena idea", "SPANISH"),
-                    ("No it is not a good idea to get lost at sea", "ENGLISH")]
-        csv_train = self.csv_dataset.load('train', skip_header = True)
+        expected = [("me gusta comer en la cafeteria".lower().split(), "SPANISH"),
+                    ("Give it to me".lower().split(), "ENGLISH"),
+                    ("No creo que sea una buena idea".lower().split(), "SPANISH"),
+                    ("No it is not a good idea to get lost at sea".lower().split(), "ENGLISH")]
+        csv_train = self.train
         output = [(doc.text, doc.label) for doc in csv_train]
         self.assertListEqual(output, expected, msg = 'Data Loading failed.')
 
-        json_train = self.json_dataset.load('train', skip_header = True)
+        self.json_dataset.load('train', skip_header = False)
+        json_train = self.json_dataset.data
         output = [(doc.text, doc.label) for doc in json_train]
         self.assertListEqual(output, expected, msg = 'Data Loading failed.')
-        self.assertIsInstance(output[0], Datapoint, msg = 'Data Loading failed gave wrong type.')
+        self.assertIsInstance(json_train[0], Datapoint, msg = 'Data Loading failed gave wrong type.')
 
     def test_build_vocab(self):
         """Test vocab building method."""
         expected = list(sorted("""me gusta comer en la cafeteria Give it to me
                    No creo que sea una buena idea No it is not a good idea to get lost at sea""".split()))
-        train = self.csv_dataset.load('train')
-        self.csv_dataset.build_label_vocab(train)
+        self.csv_dataset.build_token_vocab(self.train)
         output = list(sorted(self.csv_dataset.stoi.keys()))
         self.assertListEqual(output, expected, msg = 'Vocab building failed.')
 
@@ -57,73 +60,64 @@ class TestDataSet(unittest.TestCase):
                 No creo que sea una buena idea No it is not a good idea to get lost at sea""".split()
         test = "Yo creo que si it is lost on me".split()
         expected = train + test
-        train = self.csv_dataset.load('train')
         test = self.csv_dataset.load('test')
-        self.csv_dataset.build_vocab(train)
+        self.csv_dataset.build_vocab(self.train)
         self.csv_dataset.extend_vocab(test)
         output = list(self.csv_dataset.stoi.keys())
         self.assertListEqual(output, expected, msg = 'Vocab Extension Failed.')
 
     def test_vocab_token_lookup(self):
         '''Test looking up in vocab.'''
-        train = self.csv_dataset.load('train')
-        self.csv_dataset.build_label_vocab(train)
+        self.csv_dataset.build_label_vocab(self.train)
         expected = 0
         output = self.csv_dataset.vocab_token_lookup('me')
         self.assertEqual(output, expected, msg = 'Vocab token lookup failed.')
 
     def test_vocab_ix_lookup(self):
         '''Test looking up in vocab.'''
-        train = self.csv_dataset.load('train')
-        self.csv_dataset.build_label_vocab(train)
+        self.csv_dataset.build_label_vocab(self.train)
         expected = 'me'
         output = self.csv_dataset.vocab_ix_lookup(0)
         self.assertEqual(output, expected, msg = 'Vocab ix lookup failed.')
 
     def test_vocab_size(self):
         """Test vocab size is expected size."""
-        train = self.csv_dataset.load('train')
-        self.csv_dataset.build_vocab(train)
+        self.csv_dataset.build_vocab(self.train)
         output = self.csv_dataset.vocab_size()
         expected = 23
         self.assertEqual(output, expected, msg = 'Building vocab failed.')
 
     def test_build_label_vocab(self):
         """Test building label vocab."""
-        train = self.csv_dataset.load('train')
-        self.csv_dataset.build_label_vocab(train)
-        output = list(self.csv_dataset.ltoi.keys())
-        expected = ['SPANISH', 'ENGLISH']
+        self.csv_dataset.build_label_vocab(self.train)
+        output = list(sorted(self.csv_dataset.ltoi.keys()))
+        expected = ['ENGLISH', 'SPANISH']
         self.assertListEqual(output, expected, msg = 'Building label vocab failed.')
 
     def test_label_name_lookup(self):
         """Test looking up in label."""
-        train = self.csv_dataset.load('train')
-        self.csv_dataset.build_label_label(train)
+        self.csv_dataset.build_label_label(self.train)
         output = self.csv_dataset.label_name_lookup('SPANISH')
         expected = 0
         self.assertEqual(output, expected, msg = 'label name lookup failed.')
 
     def test_label_ix_lookup(self):
         '''Test looking up in label.'''
-        train = self.csv_dataset.load('train')
-        self.csv_dataset.build_label_label(train)
+        self.csv_dataset.build_label_label(self.train)
         output = self.csv_dataset.label_ix_lookup(0)
         expected = 'SPANISH'
         self.assertEqual(output, expected, msg = 'label ix lookup failed.')
 
     def test_label_count(self):
         """Test label size is expected."""
-        train = self.csv_dataset.load('train')
-        self.csv_dataset.build_label(train)
+        self.csv_dataset.build_label(self.train)
         expected = self.csv_dataset.label_count()
         output = 2
         self.assertEqual(output, expected, msg = 'Test that label count matches labels failed.')
 
     def test_process_label(self):
         """Test label processing."""
-        train = self.csv_dataset.load('train')
-        self.csv_dataset.build_label_vocab(train)
+        self.csv_dataset.build_label_vocab(self.train)
         expected = 0
         output = self.csv_dataset.process_label('SPANISH')
         self.assertEqual(output, expected, msg = 'Labelprocessor failed without custom processor')
@@ -155,7 +149,7 @@ class TestDataSet(unittest.TestCase):
         self.assertIsInstance(output, list, msg = 'Process document with input type list produces wrong type.')
 
         setattr(self.csv_dataset, 'preprocessor', lambda x: ["TEST" if '!' in tok else tok for tok in x])
-        expected = ["TEST" if '!' in tok else tok for tok in inputs.split()]
+        expected = ["TEST" if '!' in tok else tok for tok in inputs.lower().split()]
         output = self.csv_dataset.process_doc(inputs)
         self.assertListEqual(output, expected, msg = 'Process Document failed with preprocessor')
         self.assertIsInstance(output, list, msg = 'Process Document with preprocessor returned wrong type.')
@@ -174,24 +168,22 @@ class TestDataSet(unittest.TestCase):
 
     def test_pad(self):
         """Test padding of document."""
-        train = self.csv_dataset.load('train')
         inputs = "me gusta comer en la cafeteria"
         expected_pad = 4 * ['<pad>'] + inputs.split()
-        output_pad = list(self.csv_dataset.pad(train, length = 10))
+        output_pad = list(self.csv_dataset.pad(self.train, length = 10))
         self.assertListEqual(output_pad, expected_pad, msg = 'Padding doc failed.')
 
         expected_pad = inputs.split()[:5]
-        output_pad = list(self.csv_dataset.pad(train, length = 5))
+        output_pad = list(self.csv_dataset.pad(self.train, length = 5))
         self.assertListEqual(output_pad, expected_pad, msg = 'Trimming doc failed.')
 
         expected_pad = inputs.split()
-        output_pad = list(self.csv_dataset.pad(train, length = len(expected_pad)))
+        output_pad = list(self.csv_dataset.pad(self.train, length = len(expected_pad)))
         self.assertListEqual(output_pad, expected_pad, msg = 'Zero padding failed.')
 
     def test_onehot_encoding(self):
         """Test the onehot encoding."""
-        train = self.csv_dataset.load('train')
-        self.csv_dataset.build_label_vocab(train)
+        self.csv_dataset.build_label_vocab(self.train)
 
         test = ["Give it to me".split()]
         expected = [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1]
@@ -200,8 +192,7 @@ class TestDataSet(unittest.TestCase):
 
     def test_encoding(self):
         """Test the encoding."""
-        train = self.csv_dataset.load('train')
-        self.csv_dataset.build_label_vocab(train)
+        self.csv_dataset.build_label_vocab(self.train)
         test = ["Give it to me".split()]
         expected = [2, 4, 5, 1]
         output = self.csv_dataset.encode(test)
@@ -209,18 +200,17 @@ class TestDataSet(unittest.TestCase):
 
     def test_split(self):
         """Test splitting functionality."""
-        train = self.csv_dataset.load('train')
         expected = [4, 1]  # Lengths of the respective splits
-        train, test = self.csv_dataset.split(train, 0.8)
+        train, test = self.csv_dataset.split(self.train, 0.8)
         output = [len(train), len(test)]
         self.assertListEqual(expected, output, msg = 'Splitting with just int failed.')
 
         expected = [4, 1]
-        train, test = self.csv_dataset.split(train, [0.8, 0.2])
+        train, test = self.csv_dataset.split(self.train, [0.8, 0.2])
         output = [len(train), len(test)]
         self.assertListEqual(expected, output, msg = 'Two split values in list failed.')
 
         expected = [3, 1, 1]
-        train, dev, test = self.csv_dataset.split(train, [0.8, 0.1, 0.1])
+        train, dev, test = self.csv_dataset.split(self.train, [0.8, 0.1, 0.1])
         output = [len(train), len(dev), len(test)]
         self.assertListEqual(expected, output, msg = 'Three split values in list failed.')
