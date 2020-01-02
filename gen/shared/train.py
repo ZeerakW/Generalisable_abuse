4582b2109ab3a07a142e328805a59d03dd841607
Zeerak Waseem <zeerakw@laptop>
2020-01-02 18:57:35 +0000

    Update train and evaluation of pytorch model

--- gen/shared/train.py
+++ gen/shared/train.py
@@ -1,4 +1,5 @@
 import torch
+from collections import defaultdict
 import numpy as np
 from tqdm import tqdm
 from . import base
@@ -21,81 +22,104 @@ def run_model(library: str, train: bool, **kwargs):
             return evals
 
 
+def train_sklearn_model(arg1):
+    """TODO: Docstring for train_sklearn_model.
+
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    pass
+
+
 def train_pytorch_model(model: base.ModelType, epochs: int, batches: base.DataType, loss_func: base.Callable,
-                        optimizer: base.Callable, metrics: base.List[str], dev_batches: base.DataType = None):
+                        optimizer: base.Callable, metrics: base.Dict[str, base.Callable],
+                        dev_batches: base.DataType = None, display_metric: str = 'accuracy'):
     """Train a machine learning model.
     :model (base.ModelType): Untrained model to be trained.
     :epochs (int): The number of epochs to run.
     :batches (base.DataType): Batched training set.
     :loss_func (base.Callable): Loss function to use.
     :optimizer (bas.Callable): Optimizer function.
-    :metrics (base.List[str])): Metrics to use.
+    :metrics (base.Dict[str, base.Callable])): Metrics to use.
     :dev_batches (base.DataType, optional): Batched dev set.
+    :display_metric (str): Metric to be diplayed in TQDM iterator
     """
 
-    # TODO Look into this.
-    training_losses = []
-    training_scores = []
-    dev_losses = []
-    dev_scores = []
-    model.mode = True
+    model.train_mode = True
 
-    # TODO Get metric functions.
-    scorer = None
+    train_loss = []
+    train_scores = defaultdict(list)
 
-    for epoch in tqdm(range(epochs)):
+    for epoch in tqdm(range(epochs)):  # TODO Get TQDM to show the scores for each epoch
+
+        model.zero_grad()  # Zero out gradients
         epoch_loss = []
-        epoch_scores = []
-        model.zero_grad()
-        for X, y in batches:  # TODO Update to also use dev batches.
+        epoch_scores = defaultdict(list)
+
+        for X, y in tqdm(batches):  # TODO Get TQDM to show the scores for each batch
             scores = model(X)
+            scores = torch.argmax(scores, 1)
+
             loss = loss_func(scores, y)
+            epoch_loss.append(float(loss))
+
+            # Update steps
             loss.backward()
             optimizer.step()
-            epoch_loss.append(float(loss))
-            performance = scorer(torch.argmax(scores, 1), y)
-            # TODO Get TQDM to provide this info in output
-        training_losses.append(sum(epoch_loss))
 
-        # Predict on dev
+            for metric, scorer in metrics.items():
+                performance = scorer(scores, y)
+                epoch_scores[metric].append(performance)
+
+            # batch_performance = epoch_scores[display_metric][-1]  TODO
+        # epoch_performance = np.mean(epoch_scores[display_metric])  TODO
+
+        train_loss.append(sum(epoch_loss))
+
+        for metric in metrics:
+            train_scores[metric].append(np.mean(epoch_scores[metric]))
+
         if dev_batches:
-            dev_losses = []
-            with torch.zero_grad:
-                for devX, devY in dev_batches:
-                    dev_scores = model(devX)
-                    dev_loss = loss_func(scores, devY)
-                    dev_losses.append(dev_loss)
+            dev_loss, dev_scores = evaluate_pytorch_model(model, dev_batches, loss_func, metrics)
+            # dev_performance = dev_performance[display_metric]  TODO
 
-                    # TODO Compute dev metric
+    return train_loss, dev_loss, train_scores, dev_scores
 
-            dev_losses.append(np.mean(epoch_loss))
 
-    return losses, dev_losses,
+def evaluate_sklearn_model(arg1):
+    """TODO: Docstring for evaluate_sklearn_model.
 
-    print("Max loss: {0};Index: {1}\nMin loss: {2}; Index: {3}".format(np.max(losses), np.argmax(losses),
-                                                                       np.min(losses), np.argmin(losses)))
+    :arg1: TODO
+    :returns: TODO
+
+    """
+    pass
 
 
 def evaluate_pytorch_model(model: base.ModelType, iterator: base.DataType, loss_func: base.Callable,
-                           metrics: base.List[base.Callable]) -> base.List[float]:
+                           metrics: base.Dict[str, base.Callable]) -> base.List[float]:
     """Evaluate a machine learning model.
     :model (base.ModelType): Untrained model to be trained.
     :iterator (base.DataType): Test set to evaluate on.
     :loss_func (base.Callable): Loss function to use.
-    :metrics (base.List[str])): Metrics to use.
+    :metrics (base.Dict[str, base.Callable])): Metrics to use.
     """
-    epoch_loss = []
-    epoch_eval = []
     model.train_mode = False
+    dev_loss = []
+    dev_scores = defaultdict(list)
 
     with torch.no_grad():
         for X, y in iterator:
             scores = model(X)
-            loss = loss_func(scores, y)
             scores = torch.argmax(scores, 1)
-            m = metrics(scores, y)
 
-            epoch_loss.append(loss.item())
-            epoch_eval.append(m)
-    print(sum(epoch_eval) / len(epoch_eval), sum(epoch_loss) / len(epoch_loss))
-    return sum(epoch_eval) / len(epoch_eval), sum(epoch_loss) / len(epoch_loss)
+            loss = loss_func(scores, y)
+
+            for metric, scorer in metrics.items():
+                performance = scorer(scores, y)
+                dev_scores[metric].append(performance)
+
+            dev_loss.append(loss.item())
+
+    return np.mean(dev_loss), {m: np.mean(vals) for m, vals in dev_scores.items()}
