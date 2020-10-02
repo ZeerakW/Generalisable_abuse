2a5d76e97f8f4127945c31659cdd0c6ddd4b694f
Zeerak <z.w.butt@sheffield.ac.uk>
2020-10-02 22:49:28 +0000

    Working func

--- experiments/experiment_runner.py
+++ experiments/experiment_runner.py
@@ -143,7 +143,7 @@ if __name__ == "__main__":
     # Set seeds
     torch.random.manual_seed(args.seed)
     np.random.seed(args.seed)
-    torch.cuda.set_device(1)
+    torch.cuda.set_device(args.gpu)
 
     if 'f1' in args.metrics + [args.display, args.stop_metric]:
         for i, m in enumerate(args.metrics):
